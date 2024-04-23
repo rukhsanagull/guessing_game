f9b124192886bb2516d05f45b0add14ea17d23de
rukhsanagull <164994220+rukhsanagull@users.noreply.github.com>
2024-04-23 14:38:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "pleas guess a number:",
+}]);
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("congratulations! you guessed right number.");
+} else {
+    console.log("you guessed wrong number");
+}
